03f92611d89dc49d0333618d677bc47855e3cab8
ali7467 <jz9zfjd72h@privaterelay.appleid.com>
2026-09-01 04:06:07 +0300

    Initialize Express server in server.js
    Set up a basic Express server to serve static files and an index.html page.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+
+/*
+    FilmKeyfi sunucusu
+*/
+
+app.use(express.json());
+
+app.use(express.static(
+    path.join(__dirname)
+));
+
+app.get("/", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "index.html")
+    );
+});
+
+app.listen(PORT, () => {
+
+    console.log(
+        `FilmKeyfi çalışıyor: http://localhost:${PORT}`
+    );
+
+});
